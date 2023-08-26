fd12fbec4f8376811a3549c888fd35236039efe7
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2023-08-26 15:06:13 +0800

    Initial Commit

---
+++ recipe-blog-trial-project/recipe_blog_trial_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_blog_trial_projectApp.swift
+//  recipe-blog-trial-project
+//
+//  Created by Kyle Naranjo on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct recipe_blog_trial_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
